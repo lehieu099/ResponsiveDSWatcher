aef253317e9a142dd57227a4dbc66cdaad809b96
lehieu099 <duyhieu099a2@gmail.com>
2022-01-20 21:59:27 +0700

    config

--- src/app/service/user-service.service.ts
+++ src/app/service/user-service.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const baseUrl = "http://localhost:8000/api/users";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(params: any): Observable<any> {
+    return this.http.get(baseUrl, { params });
+  }
+
+  get(id: any): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
 }
